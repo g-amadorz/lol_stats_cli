8def03bc308226fb3278bdd95e7975423366bd0c
g-amadorz <gabe.amadorz@gmail.com>
2025-08-25 15:33:23 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module lol_stats
+
+go 1.24.6
---
+++ query.go
@@ -0,0 +1,20 @@
+package query
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func QueryPuuid(username string, tagline string) {
+
+}
+
+func QueryMatches(puuid string) {
+
+}
+
+func QueryMatch(matchId string) {
+
+}
