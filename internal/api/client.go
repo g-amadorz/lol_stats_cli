34a47bc2fb9af996f056cc889aa9e6150a2ac2ad
g-amadorz <gabe.amadorz@gmail.com>
2025-08-27 08:58:41 -0700

    file structure.

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,13 +1,51 @@
-package utils
+package api
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"io"
+	"log"
+	"lol_stats/internal/parser"
 	"net/http"
 	"os"
 )
 
-func QueryPuuid(username string, tagline string) {
+const API_BASE_PUUID_URL = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
+const API_BASE_MATCHES_URL = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"
+const API_BASE_MATCH_URL = "https://americas.api.riotgames.com/lol/match/v5/matches/"
+const API_PREFIX = "?api_key="
+
+func LoadAPIKey() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load env file")
+	}
+
+	api_key := os.Getenv("API_KEY")
+
+	if api_key == "" {
+		log.Fatal("Unable to load API_KEY")
+	}
+
+	return api_key
+
+}
+
+func QueryPuuid(username string, tagline string) error {
+
+	api_key := LoadAPIKey()
+
+	username = parser.ParseUsername(username)
+
+	response, err := http.Get(API_BASE_PUUID_URL + username + tagline + API_PREFIX + api_key)
+
+	if err != nil {
+		return fmt.Errorf("Unable to fetch PUUID: %w", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Bad request: ")
+	}
 
 }
 
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,5 +1,19 @@
 package parser
 
+func ParseUsername(username string) string {
+	parsedUsername := ""
+
+	for _, char := range username {
+		if char == ' ' {
+			parsedUsername += "%20"
+		} else {
+			parsedUsername += string(char)
+		}
+	}
+
+	return parsedUsername + "/"
+}
+
 func ParsePuuid() {
 
 }
