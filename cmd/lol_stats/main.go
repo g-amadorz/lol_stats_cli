163736a11d3e15b469d9c06cdc7d8663ff39381f
Gabriel Amador Zarza <gabe.amadorz@gmail.com>
2025-10-11 01:16:36 -0700

    stats

--- cmd/lol_stats/main.go
+++ cmd/lol_stats/main.go
@@ -2,11 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"lol_stats/internal/api"
+	"lol_stats/internal/parser"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	id, _ := api.QueryAccount("FREE PALESTINE", "tox")
+	apiKey := loadAPIKey()
+	account, err := api.QueryAccount("FREE PALESTINE", "tox", apiKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	matches, err := api.QueryMatches(account, apiKey)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println(api.QueryMatches(id))
+	games := parser.ParseMatchesInfo(matches, account)
+
+	fmt.Println(games)
+}
+
+func loadAPIKey() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load env file")
+	}
+
+	apiKey := os.Getenv("API_KEY")
+
+	if apiKey == "" {
+		log.Fatal("Unable to load API_KEY")
+	}
+
+	return apiKey
+
 }
